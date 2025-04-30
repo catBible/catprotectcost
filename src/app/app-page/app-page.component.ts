59ef62c5a5c29ce95ae8fd3620d7c52558d51aa6
thechosea <thechosea@gmail.com>
2025-04-30 16:44:01 +0000

    20

---
+++ src/app/app-page/app-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-page',
+  imports: [],
+  templateUrl: './app-page.component.html',
+  styleUrl: './app-page.component.css'
+})
+export class AppPageComponent {
+
+}
